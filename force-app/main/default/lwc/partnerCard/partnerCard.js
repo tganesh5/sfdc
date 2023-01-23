4d16c8e0a0ba6e1a499d7ca8a8dd5ff22c77ec2e
tganesh5 <theja.anaga@gmail.com>
2023-01-23 10:31:23 +0530

    HTML Directives and Component Communication

--- force-app/main/default/lwc/partnerCard/partnerCard.js
+++ force-app/main/default/lwc/partnerCard/partnerCard.js
@@ -4,14 +4,16 @@ import { LightningElement, api, wire } from 'lwc';
 
 export default class PartnerCard extends LightningElement {
 
+    // Exposing a public property 
+    @api partnerProfile; 
 
-    partnerProfile = {
+   /* partnerProfile = {
         partnerUserImg : 'https://randomuser.me/api/portraits/med/men/1.jpg',
         partnerUserCompany : 'Microsoft',
         partnerUserName : 'Sam Anderson',
         partnerType : 'Marketing Partner'
     
-    }
+    }*/
     
 
 
